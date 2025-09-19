b0c9a4540a8b80c857ec3e9730b5332a57d74f98
Leonardo Nina <leonina.2622@gmail.com>
2025-09-19 17:20:01 -0300

    codigo de login, migraciones, modelo de Usuario

--- bolsa_de_trabajo/bolsa_de_trabajo/apps/core/models.py
+++ bolsa_de_trabajo/bolsa_de_trabajo/apps/core/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Usuario(models.Model):
+    nombre = models.CharField(max_length=100)
+    correo = models.EmailField(unique=True)  # No se repite
+    contrasena = models.CharField(max_length=100)  # más adelante se puede encriptar
+
+    def __str__(self):
+        return f"{self.nombre} ({self.correo})"
+
--- bolsa_de_trabajo/bolsa_de_trabajo/apps/core/views.py
+++ bolsa_de_trabajo/bolsa_de_trabajo/apps/core/views.py
@@ -1,24 +1,25 @@
 from django.shortcuts import render
 from django.views import View
 from django.http import HttpResponse
+from .models import Usuario
 
 # Create your views here.
 
-def index(request):
-    lista_nombres = ['Leonardo','Gabriel','Santiago','Leandro']
-    lista_apellido = ['Nina','Campos Kray','Palleres','Perez Pignolo']
-    contexto = {
-        'nombres':lista_nombres,
-        'apellidos':lista_apellido,
-    }
-    return render(request, 'index.html', contexto)
-
 def home(request):
     return render(request, 'home.html', {})
-    
-def login(request):
-    return render(request, 'login.html', {})
 
 def sign_up(request):
+    if request.method == "POST":
+        nombre = request.POST.get("nombre")
+        correo = request.POST.get("correo")
+        contrasena = request.POST.get("contrasena")
+        
+        Usuario.objects.create(correo,contrasena)
+    return render(request, 'login.html', {})
+
+def login(request):
+    if request.method == "POST":
+        
+        pass
     return render(request, 'login.html', {})
 
