9be8d2eee253132041cced89fb2203899897c7fb
Leonardo Nina <leonina.2622@gmail.com>
2025-09-19 18:01:05 -0300

    el login ahora mustra lo campos escritos al confirmar

--- bolsa_de_trabajo/bolsa_de_trabajo/apps/core/views.py
+++ bolsa_de_trabajo/bolsa_de_trabajo/apps/core/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views import View
 from django.http import HttpResponse
+from django.contrib.auth import authenticate
 from .models import Usuario
 
 # Create your views here.
@@ -8,18 +9,15 @@ from .models import Usuario
 def home(request):
     return render(request, 'home.html', {})
 
-def sign_up(request):
+def login(request):
+    contexto = {}
     if request.method == "POST":
-        nombre = request.POST.get("nombre")
         correo = request.POST.get("correo")
         contrasena = request.POST.get("contrasena")
-        
-        Usuario.objects.create(correo,contrasena)
-    return render(request, 'login.html', {})
+        contexto = {
+            "correo":correo,
+            "contrasena":contrasena
+        }
 
-def login(request):
-    if request.method == "POST":
-        
-        pass
-    return render(request, 'login.html', {})
+    return render(request, 'login.html', contexto)
 
